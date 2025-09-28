7aa6aaddf7493c71b926ed4c297fac0a242cc371
chad <chadwickyiu@outlook.com>
2025-09-28 18:05:45 -0400

    Enable force static generation for robots.txt and sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from 'next';
 
+// Force static generation for robots.txt
+export const dynamic = 'force-static';
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from 'next';
 
+// Force static generation for sitemap
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
