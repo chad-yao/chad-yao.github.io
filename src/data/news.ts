abeef93ad44fe4181f823f9da91b68dc493efbb5
chang <chadwickyiu@outlook.com>
2026-02-02 14:49:02 -0500

    2/2

--- src/data/news.ts
+++ src/data/news.ts
@@ -8,9 +8,9 @@ export interface News {
 export const newsData: News[] = [
   // If you don't want to show news, just make the array empty.
   {
-    date: "",
-    title: "",
-    description: "",
+    date: "Jan. 2026",
+    title: "RE-GoT Accepted by ICRA 2026",
+    description: "A Bi-Level Language Model Framework for Reinforcement Learning.",
     link: "",
   }
 ];
