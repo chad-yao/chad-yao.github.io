8edb2a87a9f7deb0f8ccc95f517882da7439afaa
chang <chadwickyiu@outlook.com>
2026-04-19 11:47:16 -0400

    Update blog URL to include HTTPS

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -38,7 +38,7 @@ export const aboutMe: AboutMe = {
   githubUsername: "chad-yao",
   linkedinUsername: "changwei-yao-36815b212",
   twitterUsername: "changweiyao77",
-  blogUrl: "www.chad-yao.com",
+  blogUrl: "https://www.chad-yao.com",
   cvUrl: "/files/cv.pdf",
   resumeUrl: "/files/resume.pdf",
   // institutionUrl: "https://www.stanford.edu",
